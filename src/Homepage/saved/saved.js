4af334a236d583fc1508c7980eaddc5f486bca85
kislioglu <gkslogl@gmail.com>
2024-04-22 20:50:08 +0300

    pages just generated

---
+++ src/Homepage/saved/saved.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import * as React from 'react';
+import {Text} from 'react-native';
+
+export default function Saved() {
+  return (
+    <>
+      <Text>Saved</Text>
+    </>
+  );
+}
