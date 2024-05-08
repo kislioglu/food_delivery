149158eae971819f85de1b43ad2fa0b79c9239ee
kislioglu <gkslogl@gmail.com>
2024-05-08 14:07:08 +0300

    component which coverage Recipe and Recipe Vİdeo  component

--- src/Homepage/saved/saved.js
+++ src/Homepage/saved/saved.js
@@ -1,11 +1,22 @@
 /* eslint-disable prettier/prettier */
 import * as React from 'react';
-import {Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import RecipeVideo from './savedRecipeVideo';
+import Recipe from './recipe';
 
 export default function Saved() {
   return (
-    <>
-      <Text>Saved</Text>
-    </>
+    <View style={styles.bgColor}>
+      <Recipe />
+      <RecipeVideo />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  bgColor: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'white',
+  },
+});
