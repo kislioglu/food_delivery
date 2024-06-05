d5e5138a4f527224b5fdaaef69a29032b2a22150
kislioglu <gkslogl@gmail.com>
2024-06-05 15:25:48 +0300

    loading icon improved

--- src/loading/loading.js
+++ src/loading/loading.js
@@ -6,14 +6,21 @@ export default function Loading() {
   return (
     <View style={styles.loading}>
       <ActivityIndicator />
+      <Text style={styles.label}>Hold on for a second please.</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   loading: {
-    width: '100%',
+    // width: '100%',
+    // height: '85%',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  label: {
+    fontWeight: '600',
+    fontSize: 18,
+    marginTop:10,
   },
 });
